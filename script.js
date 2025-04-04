0096cc86ddf17042289d6762bce389ceecce577a
TestAcauntForMe <ranzat@bk.ru>
2025-04-04 10:52:04 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function changeWeapon(weaponType) {
-    currentWeapon = weaponType;
-    const weaponImage = document.getElementById('weaponImage');
-    if (currentWeapon === 'pistol') {
-        weaponImage.src = 'images/pistol.png';
-    } else {
-        weaponImage.src = 'images/fist.png';
-    }
-}
